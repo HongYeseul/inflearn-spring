cc1cb8d1953e52893c31ea79b8215236b0c97188
HongYeseul <ghd8119@naver.com>
2023-09-05 21:14:45 +0900

    230905 새로운 할인 정책 개발 및 의존관계가 잘못 되어있다는 것을 인지(DIP/OCP위반)

--- core/src/main/java/hello/core/Order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.Order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,11 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private  DiscountPolicy discountPolicy; // 추상에만 의존하게 됨!(인터페이스에만 의존)
+    // 해당 코드만 실행하면 구현체가 없으므로 null point exception이 발생한다.
+    // -> 누군가가 이곳에 DiscountPolicy 구현 객체를 대신 생성하고 주입 시켜줘야 한다.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {

e50115fe408074a922fd66617385ccea8c4e21f8
HongYeseul <ghd8119@naver.com>
2023-09-16 21:33:17 +0900

    조회 빈이 2개 이상일 때 해결 방법 1. @Autowired 필드명 매칭    1) 타입매칭    2) 타입 매칭 결과가 2개 이상일 때 필드명/파라미터명으로 빈 이름 추가 매칭 2. @Qualifier    1) @Qualifier 끼리 매칭    2) 빈 이름 매칭    3) NoSuchBeanDefinitionException 예외 발생 3. @Primary : 자주 사용!    @Autowired시 여러 빈이 매칭되면 @Primary가 우선순위를 가진다. *우선순위: @Qualifier > @Primary

--- core/src/main/java/hello/core/Order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -12,17 +12,16 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
 
     @Override

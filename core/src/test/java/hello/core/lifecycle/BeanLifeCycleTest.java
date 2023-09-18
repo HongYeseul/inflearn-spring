cf3b06efab0ef332e45ae4c455dcd8e660fb7ef4
HongYeseul <ghd8119@naver.com>
2023-09-19 06:40:37 +0900

    빈 생명주기 콜백: @PostConstruct, @PreDestroy 애노테이션을 사용하자 git add . 스프링에서 가장 권장하는 방법이다. 코드를 고칠 수 없는 외부 라이브러리를 초기화, 종료해야 하면  의  ,  를 사용하자.

--- core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -25,13 +25,7 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig{
 
-        // destroyMethod에는 특별한 기능이 있다.
-        // 라이브러리는 대부분 'close'나 'shutdown' 이라는 이름의 종료 메서드를 사용한다.
-        // @Bean의 `destroyMethod` 는 기본값이 `(inferred)` (추론)으로 등록되어 있다.
-        // 이 추론 기능은 `close` , `shutdown` 라는 이름의 메서드를 자동으로 호출해준다. 이름 그대로 종료 메서드를 추 론해서 호출해준다.
-        // 따라서 직접 스프링 빈으로 등록하면 종료 메서드는 따로 적어주지 않아도 잘 동작한다.
-        // 추론 기능을 사용하기 싫으면 `destroyMethod=""` 처럼 빈 공백을 지정하면 된다.
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient(){
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");

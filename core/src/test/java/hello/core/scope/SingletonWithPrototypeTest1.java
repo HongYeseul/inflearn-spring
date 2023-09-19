738e7aab5949fc7b9dd5e2de86e5bb9280acb0f1
HongYeseul <ghd8119@naver.com>
2023-09-20 06:44:16 +0900

    프로토타입 스코프-싱글톤 빈과 함께 사용시 Provider로 문제 해결 ObjectFactory, ObjectProvider / JSR-330 Provider(java표준) - ObjectFactory: 기능이 단순, 별도의 라이브러리 필요없음. 스프링 의존 - ObjectProvider: ObjectFactory 상속, 옵션, 스트림 처리 등 편의 기능 많고 별도의 라이브러리 필요없음. 스프링 의존 - Provider: Provider는 딱 필요한 DL(내부에서 스프링 컨테이너를 통해 해당 빈을 찾아서 반환) 정도 기능만 제공            기능 매우 단순, 별도의 라이브러리가 필요하지만 자바 표준이므로 스프링이 아닌 다른 컨테이너에서 사용가능 ObjectProvider는 DL을 위한 편의 기능을 많이 제공해주고 스프링 외에 별 도의 의존관계 추가가 필요 없기 때문에 편리하다. 만약(정말 그럴일은 거의 없겠지만) 코드를 스프링이 아닌 다른 컨테이너에서도 사용할 수 있어야 한다면 JSR-330 Provider를 사용해야한다. 스프링을 사용하다 보면 이 기능 뿐만 아니라 다른 기능들도 자바 표준과 스프링이 제공하는 기능이 겹칠때가 많 이 있다. 대부분 스프링이 더 다양하고 편리한 기능을 제공해주기 때문에, 특별히 다른 컨테이너를 사용할 일이 없 다면, 스프링이 제공하는 기능을 사용하면 된다.

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,8 +2,10 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -34,21 +36,19 @@ public class SingletonWithPrototypeTest1 {
         Assertions.assertThat(count1).isEqualTo(1);
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
-        int count2 = clientBean1.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+        int count2 = clientBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton") // singleton은 안 써줘도 됨(default)
     static class ClientBean{
-        private final PrototypeBean prototypeBean; // 생성시점에 주입
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanObjectProvider;
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanObjectProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
